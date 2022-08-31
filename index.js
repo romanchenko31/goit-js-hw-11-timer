896e300feed5c351e82ad6589a84a6e10dee5629
romanchenko31 <romanchenko.zhen@gmail.com>
2022-08-31 22:02:49 +0300

    add code index.js

--- index.js
+++ index.js
@@ -1,30 +1,54 @@
-new CountdownTimer({
-  selector: '#timer-1',
-  targetDate: new Date('Jul 17, 2019'),
-});
+const valueDays = document.querySelector('.timer');
+const fieldWithDay = valueDays.firstElementChild;
+const fieldWithHour = fieldWithDay.nextElementSibling;
+const fieldWithMinutes = fieldWithHour.nextElementSibling;
+const fieldWithSecond = fieldWithMinutes.nextElementSibling;
+
+let day = fieldWithDay.firstElementChild;
+let hour = fieldWithHour.firstElementChild;
+let minutes = fieldWithMinutes.firstElementChild;
+let second = fieldWithSecond.firstElementChild;
 
 
-/*
- * Оставшиеся дни: делим значение UTC на 1000 * 60 * 60 * 24, количество
- * миллисекунд в одном дне (миллисекунды * секунды * минуты * часы)
- */
-const days = Math.floor(time / (1000 * 60 * 60 * 24));
+class CountdownTimer {
+  constructor({ selector: timerName, targetDate: data }) {
+    this.timerName = timerName;
+    this.targetDate = data;
 
-/*
- * Оставшиеся часы: получаем остаток от предыдущего расчета с помощью оператора
- * остатка % и делим его на количество миллисекунд в одном часе
- * (1000 * 60 * 60 = миллисекунды * минуты * секунды)
- */
-const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  }
 
-/*
- * Оставшиеся минуты: получаем оставшиеся минуты и делим их на количество
- * миллисекунд в одной минуте (1000 * 60 = миллисекунды * секунды)
- */
-const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+  countTargetTimer() {
+    const targetMls = this.targetDate.getTime();
+    const currentMls = new Date().getTime();
+    let waitTimeMls = currentMls - targetMls;
 
-/*
- * Оставшиеся секунды: получаем оставшиеся секунды и делим их на количество
- * миллисекунд в одной секунде (1000)
- */
-const secs = Math.floor((time % (1000 * 60)) / 1000);
+    setInterval(() => {      
+      waitTimeMls-=1000;
+      const { days, hours, mins, secs } = countTime(waitTimeMls);   
+      day.textContent = days;
+      hour.textContent = hours;
+      minutes.textContent = mins;
+      second.textContent = secs;
+    }, 1000);
+   } 
+} 
+
+
+
+const targetDateTimer = new CountdownTimer({selector: '#timer-1', targetDate: new Date('Jul 17, 2021'),});
+targetDateTimer.countTargetTimer();
+
+
+function countTime(time) {
+  const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+  const hours = pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+  const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+  const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
+  return { days, hours, mins, secs };
+}
+
+
+function pad(value) {
+  return String(value).padStart(2, '0');
+}
+
