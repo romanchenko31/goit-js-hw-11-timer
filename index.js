157f232839f61178f97534c5caff1c5ffa8fbdd1
romanchenko31 <romanchenko.zhen@gmail.com>
2022-09-01 10:56:13 +0300

    edit code index.js

--- index.js
+++ index.js
@@ -1,22 +1,15 @@
-const valueDays = document.querySelector('.timer');
-const fieldWithDay = valueDays.firstElementChild;
-const fieldWithHour = fieldWithDay.nextElementSibling;
-const fieldWithMinutes = fieldWithHour.nextElementSibling;
-const fieldWithSecond = fieldWithMinutes.nextElementSibling;
-
-let day = fieldWithDay.firstElementChild;
-let hour = fieldWithHour.firstElementChild;
-let minutes = fieldWithMinutes.firstElementChild;
-let second = fieldWithSecond.firstElementChild;
-
 
 class CountdownTimer {
-  constructor({ selector: timerName, targetDate: data }) {
-    this.timerName = timerName;
-    this.targetDate = data;
-
+  constructor({ selector, targetDate }) {
+    this.getDay = document.querySelector(`${selector} .value[data-value="days"]`);
+    this.getHour = document.querySelector(`${selector} .value[data-value="hours"]`);
+    this.getMinutes = document.querySelector(`${selector} .value[data-value="mins"]`);
+    this.getSecond = document.querySelector(`${selector} .value[data-value="secs"]`);
+    this.targetDate = targetDate;
+    return this.countTargetTimer();
   }
 
+ 
   countTargetTimer() {
     const targetMls = this.targetDate.getTime();
     const currentMls = new Date().getTime();
@@ -24,31 +17,39 @@ class CountdownTimer {
 
     setInterval(() => {      
       waitTimeMls-=1000;
-      const { days, hours, mins, secs } = countTime(waitTimeMls);   
-      day.textContent = days;
-      hour.textContent = hours;
-      minutes.textContent = mins;
-      second.textContent = secs;
+      const { days, hours, mins, secs } = this.countTime(waitTimeMls);   
+      this.setTextContent({ days, hours, mins, secs });
     }, 1000);
-   } 
+} 
+   
+ setTextContent({ days, hours, mins, secs }) {
+      this.getDay.textContent = days;
+      this.getHour.textContent = hours;
+      this.getMinutes.textContent = mins;
+      this.getSecond.textContent = secs;
+ }
+  
+    countTime(time) {
+      const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+      const hours = this.pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+      const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+      const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+      return { days, hours, mins, secs };
+}
+
+
+pad(value) {
+  return String(value).padStart(2, '0');
+}
+
 } 
 
 
 
 const targetDateTimer = new CountdownTimer({selector: '#timer-1', targetDate: new Date('Jul 17, 2021'),});
-targetDateTimer.countTargetTimer();
 
 
-function countTime(time) {
-  const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
-  const hours = pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
-  const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
-  const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
-  return { days, hours, mins, secs };
-}
 
 
-function pad(value) {
-  return String(value).padStart(2, '0');
-}
+
 
